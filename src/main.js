a287405b5577e35f38da980c4e1d5a469c10725b
Andrii <dikij.andrej@gmail.com>
2025-07-18 17:57:57 +0300

    hw11

---
+++ src/main.js
@@ -0,0 +1,42 @@
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+const input = form.querySelector('input[name="search-text"]');
+
+form.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const query = input.value.trim();
+  if (!query) {
+    iziToast.error({ message: 'Please enter a search term!' });
+    return;
+  }
+
+  clearGallery();
+  showLoader();
+
+  try {
+    const data = await getImagesByQuery(query);
+    if (data.hits.length === 0) {
+      iziToast.info({
+        message:
+          'Sorry, there are no images matching your search query. Please try again!',
+      });
+    } else {
+      createGallery(data.hits);
+    }
+  } catch (error) {
+    iziToast.error({ message: 'Error fetching images. Try again later.' });
+  } finally {
+    hideLoader();
+  }
+});
